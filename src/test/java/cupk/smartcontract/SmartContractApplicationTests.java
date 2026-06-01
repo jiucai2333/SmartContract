df8ea7f7fe22b24b6f223de0e7ee763713668c5e
jiucai2333 <jiucai2333@gmail.com>
2026-06-01 19:52:39 +0800

    启动上下文测试

---
+++ src/test/java/cupk/smartcontract/SmartContractApplicationTests.java
@@ -0,0 +1,13 @@
+package cupk.smartcontract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = "spring.profiles.active=mock")
+class SmartContractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
